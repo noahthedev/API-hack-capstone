cc0f146e89699ce909958e9d809fc669d872c276
Noah Roberts <noahmroberts@gmail.com>
2021-01-12 11:11:41 -0600

    add brewery hyperlink list

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
 let locations = [];
 
+function addMarker(bounds, coordinates, map, label) {
+  var marker = new google.maps.Marker({
+     position: coordinates, 
+     map: map,
+     label: label
+  });
+  bounds.extend(marker.position);
+  return marker;
+}
+
+function generateMarkers(bounds, locations, map, labelNumber) {
+  locations.map(location => { 
+    const coordinates = {
+      lat: Number(location.latitude),
+      lng: Number(location.longitude)
+    };
+    if (location.latitude !== null || location.longitude !== null) {
+    let label = (labelNumber++).toString();
+      addMarker(bounds, coordinates, map, label);
+    };
+  })
+}
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"));
+  let bounds = new google.maps.LatLngBounds();
+  let labelNumber = 1;
+  generateMarkers(bounds, locations, map, labelNumber);
+  map.fitBounds(bounds);
+}
+
+function initBreweryList() {
+  $('#results').empty();
+  locations.map(location => {
+    if (location.latitude !== null || location.longitude !== null) {
+      $('#results').append(
+        `<li>
+           <h3>
+             <a href="${location.website_url}" target="_blank">${location.name}</a>
+          </h3>
+        </li>`
+      )
+    }
+  })
+}
+
 function getBreweries(zipCode) {
 
   const url = "https://api.openbrewerydb.org/breweries?by_postal=" + zipCode;
@@ -13,40 +59,13 @@ function getBreweries(zipCode) {
     })
     .then(responseJson => {
       locations = responseJson;
+      console.log(locations);
       initMap();
+      initBreweryList();
     }) 
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
-}
-
-function addMarker(bounds, coordinates, map) {
-  var marker = new google.maps.Marker({
-     position: coordinates, 
-     map: map
-  });
-  bounds.extend(marker.position);
-  return marker;
-}
-
-function generateMarkers(bounds, locations, map) {
-  locations.map(location => { 
-    const coordinates = {
-      lat: Number(location.latitude),
-      lng: Number(location.longitude)
-    }
-    if (location.latitude !== null || location.longitude !== null) {
-      addMarker(bounds, coordinates, map);
-    }
-  })
-}
-
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"));
-  let bounds = new google.maps.LatLngBounds();
-  generateMarkers(bounds, locations, map);
-  map.fitBounds(bounds);
-
 }
 
 function handleFormSubmit() {
